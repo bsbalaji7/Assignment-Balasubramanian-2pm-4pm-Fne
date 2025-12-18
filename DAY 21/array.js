f0fc19d6842b9edc064010191b3abdf6021db54a
BS <bsbalaji101005@gmail.com>
2025-12-18 23:18:34 +0530

    .

---
+++ DAY 21/array.js
@@ -0,0 +1,22 @@
+let fruits = ["apple", "banana", "Dragon", "mango"];
+let number = [1, 2, 3, 4, 5, 6, 7];
+let mixed_values = ["Leo", 7, true];
+
+console.log(fruits);
+
+fruits.push("guva");  
+
+console.log(fruits);
+
+
+//Adv Array method
+
+let nums = [1,2,3]
+
+nums.forEach(n => console.log(n))
+
+
+
+let doubled = nums.map(n => n*2);
+
+console.log(doubled);
---
+++ DAY 24/errors.js
@@ -0,0 +1,61 @@
+// error is an prblm that stops or affects the noraml program
+
+
+//syntax code is gramatical mistake in the sytax error
+//example
+//console.log('jn'
+
+
+//runtime error is also know as referance error
+//example
+//let x =10
+//console.log(y);
+
+
+//type error every thing is right but the data type is worng
+//let num = 5;
+//num();
+
+
+//logical error
+//let a =0;
+//let b =10;
+// console.log(a/b)
+
+
+//on top these are the basic error in java script 
+
+
+//try catch block
+
+// try {
+//     //code block
+// }catch{
+//     //code block
+// }finally {
+//     //code block wil always run
+// }
+
+
+//syntax
+// try{
+//     //risky code
+// }catch (error){
+//     //runs if code code gets error
+// }finally{
+//     //always run
+// }
+
+
+//example
+// try{
+//     let result = x +10;
+//     console.log(result)
+// }catch (error){
+//     console.log("error has Occured : ",error.message);
+//     console.log(error.name);// show the name of error
+// }finally{
+//     console.log("code run successfully")
+// }
+
+
